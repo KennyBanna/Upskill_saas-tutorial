819d89408f3abf1fae99d084bf4fc29548e24953
Oskar Norin <gravadlaxo93@gmail.com>
2017-01-10 21:47:03 +0000

    updated form input error messages

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ActiveRecord::Base
+  #Form validation
   validates :name, presence: true
   validates :email, presence: true
-  validates :comments, presence: true
+  validates :comments, length: { within: 5..180 }, presence: true
 end
