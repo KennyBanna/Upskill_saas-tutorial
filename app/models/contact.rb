d53a8c068d415ecf0ff750aa24962b4a236a3dd3
Oskar Norin <gravadlaxo93@gmail.com>
2017-01-16 20:05:14 +0000

    Added code comments

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
-  #Form validation
+  #Form validations for contact form
   validates :name, presence: true
   validates :email, presence: true
   validates :comments, length: { within: 5..180 }, presence: true
