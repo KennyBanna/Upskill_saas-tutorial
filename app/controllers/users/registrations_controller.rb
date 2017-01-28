378f6ec407fafcf48c5f96e60124f97ae54ce0f9
Oskar Norin <gravadlaxo93@gmail.com>
2017-01-28 09:46:21 +0000

    Added flash messages for selecting membership plan

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController 
+  before_action :select_plan, only: :new
+  
   # Extend default devise gem behaviour so that users 
   # signing up with pro account (Plan ID: 2)
   # Save with a special function.
@@ -15,4 +17,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
+  
+  private 
+    def select_plan
+      unless(params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = 'Please select a plan to sign up with'
+        redirect_to root_path
+      end
+    end
 end
