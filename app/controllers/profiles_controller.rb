a61fe43e6a393cdf89cd0e1fff3d82d4e7583aa7
Oskar Norin <gravadlaxo93@gmail.com>
2017-01-30 12:23:42 +0000

    Added profile form view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
   #GET request to users/:user_id/profile/new
   def new
-    #Render blank profile details form 
+    @profile = Profile.new
+    #Render profile details form 
   end
 end
